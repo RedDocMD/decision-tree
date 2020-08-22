dccab8cc342e4c21b7542b82c6ad091194c1c806
Deep Majumder <deep.majumder2019@gmail.com>
2020-08-22 18:18:04 +0530

    Parser completed

--- parser/parser.go
+++ parser/parser.go
@@ -43,22 +43,55 @@ func ParseFile(filename string) (*InputData, error) {
 		if err != nil {
 			return nil, err
 		}
+		size := uint(len(record)) - 1
 		if idx == 0 {
-			size := uint(len(record)) - 1
 			inputData = newInputData(size)
 			for i := uint(0); i < size; i++ {
 				inputData.attributeNames[i] = record[i]
 				inputData.attributes[i].name = record[i]
 			}
 			inputData.resultName = record[size]
+		} else {
+			if len(inputData.rows) == idx-1 {
+				inputData.doubleRows()
+			}
+			for i := uint(0); i < size; i++ {
+				inputData.rows[idx-1].values[inputData.attributeNames[i]] = record[i]
+				if !containsString(inputData.attributes[i].values, record[i]) {
+					inputData.attributes[i].values = append(inputData.attributes[i].values, record[i])
+				}
+			}
+			inputData.rows[idx-1].result = true
+			if record[size] == "no" {
+				inputData.rows[idx-1].result = false
+			}
 		}
+		idx++
 	}
+	return inputData, nil
 }
 
 func newInputData(size uint) *InputData {
 	inputData := new(InputData)
 	inputData.attributeNames = make([]string, size)
 	inputData.attributes = make([]Attribute, size)
+	for _, attribute := range inputData.attributes {
+		attribute.values = make([]string, 0)
+	}
 	inputData.rows = make([]Row, 100)
 	return inputData
+}
+
+func (data *InputData) doubleRows() {
+	newRows := make([]Row, len(data.rows))
+	data.rows = append(data.rows, newRows...)
+}
+
+func containsString(arr []string, elem string) bool {
+	for _, val := range arr {
+		if elem == val {
+			return true
+		}
+	}
+	return false
 }
